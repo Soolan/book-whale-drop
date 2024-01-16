253b7b45cae33d9f1e0089674953d72695ca7e6b
Soolan <info@soolan.com>
2024-01-16 10:18:28 +0800

    [ch4] generate the whale module

--- projects/dashboard/src/app/app.module.ts
+++ projects/dashboard/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { WhaleModule } from './whale/whale.module';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    WhaleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/dashboard/src/app/whale/whale.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WhaleRoutingModule } from './whale-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WhaleRoutingModule
+  ]
+})
+export class WhaleModule { }
