1ef40ef14d57c3f14da6ddc1706f22ad86b68934
Soolan <info@soolan.com>
2024-01-16 10:31:44 +0800

    [ch4] generate the whale landing

--- projects/dashboard/src/app/whale/whale.module.ts
+++ projects/dashboard/src/app/whale/whale.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WhaleRoutingModule } from './whale-routing.module';
+import { LandingComponent } from './landing/landing.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingComponent
+  ],
   imports: [
     CommonModule,
-    WhaleRoutingModule
+    WhaleRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class WhaleModule { }
