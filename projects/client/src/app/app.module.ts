6bf3bd58be62af55c30b8646949ea98e4920aed7
Soolan <info@soolan.com>
2024-02-06 17:30:58 +0800

    [ch11] add the missing scripts

--- projects/client/src/app/app.module.ts
+++ projects/client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,6 +28,7 @@ import { AnimationDirective } from './directives/animation.directive';
 import { GltfModelDirective } from './directives/gltf-model.directive';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent,
     LandingComponent,
