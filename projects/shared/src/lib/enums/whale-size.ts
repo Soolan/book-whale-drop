a5fc4984ffcfbe04e6fef5453cd85763a47de129
Soolan <info@soolan.com>
2024-02-10 19:46:56 +0800

    [CLD] refactor the whale size enum

--- projects/shared/src/lib/enums/whale-size.ts
+++ projects/shared/src/lib/enums/whale-size.ts
@@ -1,7 +1,8 @@
 export enum WhaleSize {
-  Small,
-  Medium,
-  Large,
-  Huge,
-  Gigantic,
+  Small,   // 10 meters (i.e Minke Whale: fit in shopping malls)
+  Medium,  // 15 meters (i.e Humpback Whale)
+  Large,   // 20 meters (i.e Sperm Whale)
+  Huge,    // 30 meters (i.e Blue Whale)
+  Gigantic,// 100 meters (i.e Meta Whale)
+  Colossal // 500 meters (i.e WTF Whale)
 }
